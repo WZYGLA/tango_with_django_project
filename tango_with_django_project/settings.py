4302fc07034cbe0462471c5acecc1b8174b64808
WZYGLA <2527931w@student.gla.ac.uk>
2022-02-18 23:16:05 +0000

    third commit

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -14,8 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -29,6 +30,11 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+STATICFILES_DIRS = [STATIC_DIR, ]
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
+
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -53,20 +59,22 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'tango_with_django_project.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
+{
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': [TEMPLATE_DIR, ],
+    'APP_DIRS': True, 
+    'OPTIONS': {
+        'context_processors':
+        [ 'django.template.context_processors.debug', 
+        'django.template.context_processors.request', 
+        'django.contrib.auth.context_processors.auth', 
+        'django.contrib.messages.context_processors.messages',
+        'django.template.context_processors.media',
+        ],
     },
+},
 ]
+
 
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
